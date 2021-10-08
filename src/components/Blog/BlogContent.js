9e6ad1ad1000c433ed79428b2f7503cd4b3948fb
unknown <dimazu65@gmail.com>
2021-10-07 21:59:38 -0400

    Adding record to the array and save it to localstorage

--- src/components/Blog/BlogContent.js
+++ src/components/Blog/BlogContent.js
@@ -4,89 +4,102 @@ import "./BlogContent.css"
 import { AddPostForm } from "./components/AddPostForm";
 import { BlogCard } from "./components/BlogCard"
 export class BlogContent extends Component {
+  state = {
+    showAddForm: false,
+    blogArr: JSON.parse(localStorage.getItem("blogPosts")) || posts,
+  };
 
-    state = {
-        showAddForm: false,
-        blogArr: JSON.parse(localStorage.getItem('blogPosts')) || posts
-    };
+  likePost = (pos) => {
+    const temp = [...this.state.blogArr];
+    temp[pos].liked = !temp[pos].liked;
 
-    likePost = pos => {
-        const temp =[...this.state.blogArr]
-        temp[pos].liked = !temp[pos].liked
-// Ни хера не понятно как копируется массив         
-// изменяется и temp и blogArr
-// Странно...
-    
- //   console.log('Source array', this.state.blogArr)
- //   console.log('Temp array', temp)
+    this.setState({
+      blogArr: temp,
+    });
+    localStorage.setItem("blogPosts", JSON.stringify(temp));
+  };
 
-        this.setState({
-            blogArr: temp
-        })
-     localStorage.setItem('blogPosts',JSON.stringify(temp))   
-    } 
-
-    deletePost = pos =>{
-       if (window.confirm(`Are you sure to remove ${this.state.blogArr[pos].title} ?`)) { 
-       const temp=[...this.state.blogArr]
-       temp.splice(pos,1)
-       this.setState ({
-          blogArr : temp
-       })
-      localStorage.setItem('blogPosts', JSON.stringify(temp))  
+  deletePost = (pos) => {
+    if (window.confirm(`Remove ${this.state.blogArr[pos].title} ?`)) {
+      this.setState((state) => {
+        const temp = [...this.state.blogArr];
+        temp.splice(pos, 1);
+        localStorage.setItem("blogPosts", JSON.stringify(temp));
+        return {
+          blogArr: temp,
+        };
+      });
     }
-    }
+  };
 
-    triggerShowAddForm = () => {
-        this.setState({
-            showAddForm :true
-        })
-    }
+  triggerShowAddForm = () => {
+    this.setState({
+      showAddForm: true,
+    });
+  };
 
-    triggerHideAddForm = () => {
-        this.setState({
-            showAddForm :false
-        })  
-    }
+  triggerHideAddForm = () => {
+    this.setState({
+      showAddForm: false,
+    });
+  };
 
+  handleEscape = (e) => {
+    if (e.key === "Escape" && this.state.showAddForm) this.triggerHideAddForm();
+  };
 
-    render(){
-        
-        const blogPosts = this.state.blogArr.map((item, pos )=> {
-            return (
-               <BlogCard 
-                 key={item.id}
-                 title={item.title}
-                 description = {item.description}
-                 liked = {item.liked}
-                 likePost = {() => this.likePost(pos)}
-                 deletePost= {()=> this.deletePost(pos)}
-               />
-            )
-        });
-         return(
-            <> 
+  // добавляю запись в массив после AddPostForm
+  addNewBlogPost = (blogPost) => {
+    this.setState((state) => {
+      const temp = [...state.blogArr];
+      temp.push(blogPost);
+      localStorage.setItem("blogPosts", JSON.stringify(temp));
+      return {
+        blogArr: temp,
+      };
+    });
 
-           {
-               this.state.showAddForm ? <AddPostForm triggerHideAddForm={this.triggerHideAddForm} /> : null
-           } 
-            
-           
-            {
-                
-                <>
-                    <h1>Simple Blog</h1>
-                    <button className="blackBtn" onClick={this.triggerShowAddForm}>
-                        New post
-                    </button>
-                    <div className="posts">  
-                        {blogPosts} 
-                    </div>
-                </>
-               
-            }
-             </>  
-    
-         );
-    }
+    this.triggerHideAddForm();
+  };
+
+  componentDidMount() {
+    window.addEventListener("keyup", this.handleEscape);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("keyup", this.handleEscape);
+  }
+  render() {
+    const blogPosts = this.state.blogArr.map((item, pos) => {
+      return (
+        <BlogCard
+          key={item.id}
+          title={item.title}
+          description={item.description}
+          liked={item.liked}
+          likePost={() => this.likePost(pos)}
+          deletePost={() => this.deletePost(pos)}
+        />
+      );
+    });
+    return (
+      <div className="blogPage">
+        {this.state.showAddForm ? (
+          <AddPostForm
+            blogArr={this.state.blogArr}
+            addNewBlogPost={this.addNewBlogPost}
+          />
+        ) : null}
+        <>
+          <h1>Simple Blog</h1>
+          <div className="addNewBtn">
+            <button className="blackBtn" onClick={this.triggerShowAddForm}>
+              New post
+            </button>
+          </div>
+          <div className="posts">{blogPosts}</div>
+        </>
+      </div>
+    );
+  }
 };
--- src/components/Blog/components/AddPostForm.js
+++ src/components/Blog/components/AddPostForm.js
@@ -1,25 +1,71 @@
 import "./AddPostForm.css"
 import HighlightOffIcon from '@mui/icons-material/HighlightOff';
-export const AddPostForm = ({triggerHideAddForm}) => {
-  return (
-      <>
-        <form action="" className="addPostForm">
-             <h2>New post</h2>
-             <button className="hideBtn" onClick={triggerHideAddForm}>
-                 <HighlightOffIcon />
-             </button>
-             <div>
-                <input className="addFormInput" type="text" name = "pstTitle" placeholder="Title"/>
-             </div>
-             <div>
-                 <textarea className="addFormInput" name="pstDesc" placeholder="Description"></textarea>
-              </div>
-             <div>
-                 <button onClick={triggerHideAddForm} className="blackBtn" type="button">Add post</button>
-             </div>
-            </form>
-            <div onClick={triggerHideAddForm} className="overlay"></div>
-            </>
-            )
+import { Component } from "react";
+export class AddPostForm extends Component {
+    state ={
+        postTitle:'',
+        postDesc: '',
+    }
+ onChangeTitle = (e) => {
+   this.setState({
+       postTitle: e.target.value
+   }) 
+ }
+ onChangeDesc = (e) => {
+    this.setState({
+        postDesc: e.target.value
+    }) 
+  }
+  createPost = () => {
+      const post ={
+        id: this.props.blogArr.length + 1,
+        title: this.state.postTitle,
+        description: this.state.postDesc,
+        liked : false
+      }
+      
+      this.props.addNewBlogPost(post)
+  }
+ render() {
+    const triggerHideAddForm = this.props.triggerHideAddForm 
+    return (
+        <>
+          <form action="" className="addPostForm">
+               <h2>New post</h2>
+               <button className="hideBtn" onClick={triggerHideAddForm}>
+                   <HighlightOffIcon />
+               </button>
+               <div>
+                  <input 
+                    className="addFormInput" 
+                    type="text" 
+                    name = "pstTitle" 
+                    placeholder="Title"
+                    value = {this.state.postTitle}
+                    onChange={this.onChangeTitle}
+                  />
+               </div>
+               <div>
+                   <textarea 
+                     className="addFormInput" 
+                     name="pstDesc" 
+                     placeholder="Description"
+                     value={this.state.postDesc}
+                     onChange={this.onChangeDesc}
+                   />
+                </div>
+               <div>
+                   <button 
+                     onClick={this.createPost} 
+                     className="blackBtn" 
+                     type="button">
+                         Add post
+                   </button>
+               </div>
+              </form>
+              <div onClick={triggerHideAddForm} className="overlay"></div>
+              </>
+              )
+ }
             
 }
